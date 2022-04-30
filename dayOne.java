a60f1dcc58a10b70d1c3abcc4ee1c5f23ad9d931
Mazda-Shahzadi-Personal <104691634+Mazda-Shahzadi-Personal@users.noreply.github.com>
2022-05-01 03:36:22 +1000

    Add files via upload

---
+++ dayOne.java
@@ -0,0 +1,8 @@
+class dayOne {
+
+    public static void main(String[] args){
+        System.out.println("day one of my habbit to program every day :).");
+        int number = 7;
+        System.out.println("My number is " + number);
+    }
+}
