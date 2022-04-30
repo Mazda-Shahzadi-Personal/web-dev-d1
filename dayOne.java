3b4d1b1fc00c08f2b055c710d26239781bcfe63b
Mazda-Shahzadi-Personal <104691634+Mazda-Shahzadi-Personal@users.noreply.github.com>
2022-05-01 03:36:33 +1000

    Delete dayOne.java

--- dayOne.java
+++
@@ -1,8 +0,0 @@
-class dayOne {
-
-    public static void main(String[] args){
-        System.out.println("day one of my habbit to program every day :).");
-        int number = 7;
-        System.out.println("My number is " + number);
-    }
-}
